8820a80a34140dbb974d101f43239dc37ac7d75f
SureshRamavath <sureshbs.j2ee@gmail.com>
2025-02-17 11:35:18 +0530

    Code added for Kafka integration.

--- src/main/java/com/example/CustomerService/CustomerService.java
+++ src/main/java/com/example/CustomerService/CustomerService.java
@@ -1,6 +1,7 @@
 package com.example.CustomerService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -20,4 +21,20 @@ import reactor.core.publisher.Mono;
                     .doOnComplete(() -> System.out.println("Successfully retrieved all products"))
                     .doOnError(error -> System.out.println("Error retrieving products"));
         }
-}
+        @KafkaListener(topics = "order-topic", groupId = "customer-group")
+        public void consumeOrderEvent(OrderEvent event) {
+            System.out.println("Validating customer for Order ID: " + event.getProductId());
+
+            repository.findById(event.getCustomerId())
+                    .doOnSuccess(customer -> {
+                        if (customer != null) {
+                            System.out.println("Customer exists: " + customer.getName());
+                        } else {
+                            System.err.println("Customer ID not found: " + event.getCustomerId());
+                        }
+                    })
+                    .doOnError(error -> System.err.println("Failed to validate customer: " + error.getMessage()))
+                    .subscribe();
+        }
+
+    }
